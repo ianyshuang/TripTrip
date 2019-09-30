e987cca68a3d49e8ad6fe136223355783f3bc51e
ianyshuang <ianyshuang@gmail.com>
2019-09-30 23:59:08 +0800

    fix bug of oauth redirect uri

--- routes/facebook.js
+++ routes/facebook.js
@@ -2,17 +2,16 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const issueToken = require('../utils/issue_token')
+const redirectBaseUrl = process.env.NODE_ENV === 'production' ? 'https://triptrip-backend.herokuapp.com' : 'http://localhost:8080/#/'
 
 router.get('/', passport.authenticate('facebook', { scope: ['email', 'public_profile'], session: false }))
 router.get(
   '/redirect',
   (req, res, next) => {
     passport.authenticate('facebook', (err, user, info) => {
-      if (err) { return res.redirect('http://localhost:8080/#/redirect?error=true') }
+      if (err) { return res.redirect(`${redirectBaseUrl}/redirect?error=true`) }
       if (Object.keys(info).length !== 0) {
-        return res.redirect(
-          `http://localhost:8080/#/redirect?status=${info.status}`
-        )
+        return res.redirect(`${redirectBaseUrl}/redirect?status=${info.status}`)
       }
       // custom callback 記得要自己 req.logIn 把 user 放到 req.user 中，並記得要加上 { session: false }
       req.logIn(user, { session: false }, (err) => {
@@ -23,7 +22,7 @@ router.get(
   },
   issueToken,
   (req, res) => {
-    res.redirect('http://localhost:8080/#/redirect')
+    res.redirect(`${redirectBaseUrl}/redirect`)
   }
 )
 
--- routes/google.js
+++ routes/google.js
@@ -2,17 +2,16 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const issueToken = require('../utils/issue_token')
+const redirectBaseUrl = process.env.NODE_ENV === 'production' ? 'https://triptrip-backend.herokuapp.com' : 'http://localhost:8080/#/'
 
 router.get('/', passport.authenticate('google', { scope: ['profile', 'email'], session: false }))
 router.get(
   '/redirect',
   (req, res, next) => {
     passport.authenticate('google', (err, user, info) => {
-      if (err) { return res.redirect('http://localhost:8080/#/redirect?error=true') }
+      if (err) { return res.redirect(`${redirectBaseUrl}/redirect?error=true`) }
       if (Object.keys(info).length !== 0) {
-        return res.redirect(
-          `http://localhost:8080/#/redirect?status=${info.status}`
-        )
+        return res.redirect(`${redirectBaseUrl}/redirect?status=${info.status}`)
       }
       req.logIn(user, { session: false }, (err) => {
         if (err) return next(err)
@@ -22,7 +21,7 @@ router.get(
   },
   issueToken,
   (req, res) => {
-    res.redirect('http://localhost:8080/#/redirect')
+    res.redirect(`${redirectBaseUrl}/redirect`)
   }
 )
 
